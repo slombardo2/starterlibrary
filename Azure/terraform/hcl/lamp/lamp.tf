f73970b385e2cbaeb2d24cc8cf19349329ed9582
Sean Lombardo <sean.lombardo@ibm.com>
2020-06-24 00:47:15 +0000

    Changing ports

--- Azure/terraform/hcl/lamp/lamp.tf
+++ Azure/terraform/hcl/lamp/lamp.tf
@@ -280,7 +280,8 @@ resource "azurerm_lb_nat_rule" "azlb" {
   name                           = "VM-${count.index}"
   protocol                       = "tcp"
   frontend_port                  = "5000${count.index + 1}"
-  backend_port                   = "${element(var.remote_port["${element(keys(var.remote_port), count.index)}"], 1)}"
+#  backend_port                   = "${element(var.remote_port["${element(keys(var.remote_port), count.index)}"], 1)}"
+  backend_port                   = "5000${count.index + 1}"
   frontend_ip_configuration_name = "${var.frontend_name}"
 }
 
@@ -289,9 +290,10 @@ resource "azurerm_lb_probe" "azlb" {
   resource_group_name = "${azurerm_resource_group.azlb.name}"
   loadbalancer_id     = "${azurerm_lb.azlb.id}"
   name                = "${element(keys(var.lb_port), count.index)}"
-# protocol            = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 1)}"
+#  protocol            = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 1)}"
   protocol            = "tcp"
-  port                = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 2)}"
+#  port                = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 2)}"
+  port                = "22"
   interval_in_seconds = "${var.lb_probe_interval}"
   number_of_probes    = "${var.lb_probe_unhealthy_threshold}"
 }
@@ -301,10 +303,12 @@ resource "azurerm_lb_rule" "azlb" {
   resource_group_name            = "${azurerm_resource_group.azlb.name}"
   loadbalancer_id                = "${azurerm_lb.azlb.id}"
   name                           = "${element(keys(var.lb_port), count.index)}"
-# protocol                       = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 1)}"
+#  protocol                       = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 1)}"
   protocol                       = "tcp"
-  frontend_port                  = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 0)}"
-  backend_port                   = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 2)}"
+#  frontend_port                  = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 0)}"
+#  backend_port                   = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 2)}"
+  frontend_port                  = "5000${count.index + 1}"
+  backend_port                   = "5000${count.index + 1}"
   frontend_ip_configuration_name = "${var.frontend_name}"
   enable_floating_ip             = false
   backend_address_pool_id        = "${azurerm_lb_backend_address_pool.azlb.id}"
